48698c39bfda2c091230b59680027011fc8ee5c1
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-05 23:19:47 -0300

    ESTILIZANDO E APLICANDO UMA LÓGICA

---
+++ src/components/errorAlert/ErrorValue.tsx
@@ -0,0 +1,14 @@
+import styles from './errorAlert.module.scss'
+
+const ErrorValue = () => {
+  return (
+    <>
+    <div className={styles.string_alert}>
+        <h1>É necessário escrever uma cidade</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default ErrorValue
--- src/components/searchBox/SearchBox.tsx
+++ src/components/searchBox/SearchBox.tsx
@@ -2,10 +2,22 @@ import { useState } from "react";
 import { CiMicrophoneOn } from "react-icons/ci";
 import { CiMicrophoneOff } from "react-icons/ci";
 import styles from './searchBox.module.scss'
+import ErrorValue from "../errorAlert/ErrorValue";
 
 const SearchBox = () => {
 
   const [micOn, setMicOn] = useState<boolean>(false)
+
+  const [searchCity, setSearchCity] = useState<string>('Tokyo')
+  const [showError, setShowError] = useState<boolean>(false)
+
+  function handleSearchData() {
+    if(searchCity !== "") {
+       setShowError(true)
+    }
+  }
+
+
 
 
   return (
@@ -13,9 +25,22 @@ const SearchBox = () => {
 
     <section className={styles.climate_area}>
       <div className={styles.search_tools}>
-        <input type="text" placeholder="Tokyo"/>
+        <input onChange={(e) : void => setSearchCity(e.target.value)} type="text" placeholder={searchCity}/>
         <button className={styles.mic_btn} onClick={() : void => setMicOn(!micOn)}>{micOn ? <CiMicrophoneOn size={30} /> : <CiMicrophoneOff size={30} />}</button>
       </div>
+
+      <section>
+        <div>
+          <h3>Tokyo</h3>
+          <div>13°C</div>
+        </div>
+      </section>
+
+      <button onClick={handleSearchData}>Buscar</button>
+
+      {showError ? <ErrorValue /> : ''}
+
+
     </section>
     
     </>
